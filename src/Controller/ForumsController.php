8e654e068ddd38a0231b83446f497aee25ec40b1
Osmant7 <osmantoy035@gmail.com>
2021-06-15 13:56:02 +0200

    lastUpdate

--- src/Controller/ForumsController.php
+++ src/Controller/ForumsController.php
@@ -10,25 +10,23 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
-class ForumsController extends AbstractController
-{
+class ForumsController extends AbstractController {
      /**
      * @Route("/CreerUnSujet/{id}", name="New_Forum")
      */
-    public function new(Request $request,SousCategories $souscategories): Response {
-        dump($souscategories);
-        //$forum = new Forums();*/
+    public function new(Request $request, int $id): Response {
+        $forum = new Forums();
         
-        //$form = $this->createForm(ForumType::class, $forum);
+        $form = $this->createForm(ForumType::class, $forum);
 
         // on met à jour l'objet $form avec les données saisies
-        /*$form->handleRequest($request);
+        $form->handleRequest($request);
 
         // On s'assure que le formulaire à été soumis et que les données sont cohérentes 
         if($form->isSubmitted() && $form->isValid()) {
             $forum->setCreatedAt(new \DateTime());
-            //$forum->setSouscategories($souscategories);
+            $souscategories = $this->getDoctrine()->getRepository(SousCategories::class)->find($id);
+            $forum->setSouscategories($souscategories);
             // on récupère une instance de l'Entity Manager gràce à la méthode getDoctrine()
             //issu de la class AbstractController
             $em = $this->getDoctrine()->getManager();
@@ -36,9 +34,9 @@ class ForumsController extends AbstractController
             $em->flush(); // on synchronise l'objet ajouté a l'entity manager avec la BDD
             // return $this->redirectToRoute('souscategories');
 
-        }*/
+        }
         return $this->render('Forums/CreerForums.html.twig', [
-            //'form' => $form->createView()
+            'form' => $form->createView()
         ]);
     }
 }
