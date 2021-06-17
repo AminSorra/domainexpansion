43c9523ca383b14f86efb3e30f6f0d3a51d32ec9
Ota <59179854+Rihito-san@users.noreply.github.com>
2021-06-17 14:35:12 +0200

    OtaFront

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -15,7 +16,7 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', TextType::class)
+            ->add('email', EmailType::class)
             ->add('username', TextType::class)
             ->add('password', PasswordType::class)
             ->add('confirm_password', PasswordType::class)
